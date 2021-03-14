f69a06675900a955c39e626e1dc53f9078aba647
hangze liu <hangze.liu@gmail.com>
2021-03-14 23:43:41 +0000

    still have some problems

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 //------------------------------
 
 // Implement the class methods
-class Rot13 {
+class Rot13 { 
     const int rotation{13};
     std::string text;
 
@@ -15,8 +15,57 @@ public:
     void operator>>(std::string&);
     friend void operator<<(std::string&, Rot13&);
 };
-
-
+Rot13::Rot13(std::string msg){
+if(islower(msg)){
+text= msg;
+}
+else{
+text= " ";
+}
+}
+bool Rot13::islower(std::string str){
+int i=0;
+while(i<str.length()){
+if(str[i]>='a'&&str[i]<='z'&&str[i]==' '){
+i++;
+}
+else{
+return false;
+}
+}
+return true;
+}
+bool Rot13::operator ! () {
+  if(this->text.size()==0)
+return true;
+  else
+return false;
+}
+void Rot13::operator>>(string& encrypted){
+string s = "";
+for(int i=0;i<text.length();i++){
+if(text[i]>='a'&&text[i]<='z'){
+int p=text[i]-'a';
+p=(p+13)%26;
+s+=p+'a';
+}
+else{
+s+=text[i];
+}
+}
+encrypted=s;
+}
+friend void operator<<(std::string&out,Rot13& r){
+string s;
+for(int i=0;i<text.length();i++){
+if(text[i]>='a'&&text[i]<='z'){
+int p=text[i]-'a';
+p=(p+13)%26;
+s+=p+'a';
+}
+else{
+s+=text[i];
+};
 //------------------------------
 //   DO NOT MODIFY TEST CASES
 //------------------------------
